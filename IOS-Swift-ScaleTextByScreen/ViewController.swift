5d41925fbd1c9055dab65ff4ff0d4e14f3e43854
Pooya Hatami <pooya@node-1w7jr9qro06sykejacw9hdpxg.ipv6.telus.net>
2018-09-11 05:10:17 -0700

    done

--- IOS-Swift-ScaleTextByScreen/ViewController.swift
+++ IOS-Swift-ScaleTextByScreen/ViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var label1 : UILabel!
+    @IBOutlet weak var label2 : UILabel!
+    @IBOutlet weak var label3 : UILabel!
+    
+    let relativeFontConstant1:CGFloat = 0.046
+    let relativeFontConstant3:CGFloat = 0.060
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        label1.font = label1.font.withSize(self.view.frame.height * relativeFontConstant1)
+        label3.font = label3.font.withSize(self.view.frame.height * relativeFontConstant3)
     }
 
 
